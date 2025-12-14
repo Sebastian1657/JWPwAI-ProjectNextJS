0f562ecb523b80818d780f36dc94f63f60c97a60
Sebastian1657 <miler.sebastian1101@gmail.com>
2025-12-14 19:42:48 +0100

    cz.2 Next.js

--- app/main-header.js
+++ app/main-header.js
@@ -2,7 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logoImg from '../assets/icons/logo.png';
 import classes from './main-header.module.css';
-import MainHeaderBackground from './components/main-header-background';
+import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader({children}) {
   return (
@@ -17,10 +18,14 @@ export default function MainHeader({children}) {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">
+                Browse Meals
+              </NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">
+              Foodies Community
+              </NavLink>
             </li>
           </ul> 
         </nav>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,38 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import ImageSlideshow from './components/images/image-slideshow';
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Laboratorium nr 11 - projekt startowy
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>Nextlevel Food for Nextlevel Foodies</h1>
+            <p> Taste & share food form all over the world!</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>Nextlevel Food is a platform for foodie to share their favorite recipes with the world. It&apos;s a place to discover new dishes, and to connect with other food lovers.</p>
+          <p>Nextlevel Food is a place to discover new dishes, and to connect with other food lovers.</p>
+        </section>
+        <section className={classes.section}>
+          <h2>Why Nextlevel Food?</h2>
+          <p>Nextlevel Food is a platform for foodie to share their favorite recipes with the world. It&apos;s a place to discover new dishes, and to connect with other food lovers.</p>
+          <p>Nextlevel Food is a place to discover new dishes, and to connect with other food lovers.</p>
+        </section>
+      </main>
+    </>
   );
 }
